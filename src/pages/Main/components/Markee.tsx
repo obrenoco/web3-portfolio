c96704be3717fb1aac6cc669a284ca2f3a2bd27e
Breno Romeiro <breno.romeiro@Brenos-MacBook-Pro.local>
2024-03-07 18:13:33 -0300

    add markee

--- src/pages/Main/components/Markee.tsx
+++ src/pages/Main/components/Markee.tsx
@@ -1,18 +1,27 @@
 import { SparkIcon } from "../../../assets/icons/spark";
 
-export const Markee = () => (
-  <section className="w-full h-20 bg-primary text-white text-3xl font-bold flex items-center [&>*]:uppercase text-nowrap">
-    <div className="flex gap-8 pl-body">
-      <h3>Discuss your ideias</h3>
-      <SparkIcon className="inline w-8 h-8" />
-    </div>
-    <div className="flex gap-8 pl-body">
-      <h3>Discuss your ideias</h3>
-      <SparkIcon className="inline w-8 h-8" />
-    </div>
-    <div className="flex gap-8 pl-body">
-      <h3>Discuss your ideias</h3>
-      <SparkIcon className="inline w-8 h-8" />
-    </div>
-  </section>
-);
+const El = () => {
+  const jsxElements = [];
+  for (let index = 0; index < 10; index++) {
+    jsxElements.push(
+      <>
+        <h3>Web3 Portfolio</h3>
+        <SparkIcon className="inline w-8 h-8" />
+      </>
+    );
+  }
+  return jsxElements;
+};
+
+export const Markee = () => {
+  return (
+    <section className="marquee w-full h-20 bg-white text-primary text-3xl font-bold flex items-center [&>*]:uppercase text-nowrap">
+      <div className="marquee-content scroll flex gap-8">
+        <El />
+      </div>
+      <div className="marquee-content scroll flex gap-8 ml-body">
+        <El />
+      </div>
+    </section>
+  );
+};
