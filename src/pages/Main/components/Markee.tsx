0353f326d773199d8b5ae6e75d8041fbc92c279c
Breno Romeiro <breno.romeiro@Brenos-MacBook-Pro.local>
2024-03-12 20:35:40 -0300

    refactor

--- src/pages/Main/components/Markee.tsx
+++ src/pages/Main/components/Markee.tsx
@@ -15,7 +15,7 @@ const El = () => {
 
 export const Markee = () => {
   return (
-    <section className="w-auto h-20 bg-black text-white text-xl md:text-5xl font-black flex items-center [&>*]:uppercase text-nowrap">
+    <section className="w-auto h-20 bg-black text-white text-xl md:text-5xl font-black flex items-center [&>*]:uppercase text-nowrap ">
       <div className="scroll flex gap-8 items-center">
         <El />
       </div>
